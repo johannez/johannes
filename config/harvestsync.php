9a6a95aa5bd1089fe831c859b7f4b9ecf958a999
Johannes Schmidt <mail@johannez.com>
2017-11-07 15:51:04 -0800

    Fixed homepage

--- config/harvestsync.php
+++ config/harvestsync.php
@@ -2,9 +2,9 @@
 
 return [
     'account' => [
-        'id' => '2tabs',
-        'url' => 'https://2tabs.harvestapp.com',
-        'user' => 'mail@2tabs.com',
+        'id' => 'johannez',
+        'url' => 'https://johannez.harvestapp.com',
+        'user' => 'mail@johannesschmidt.ca',
         'password' => 'eyJpdiI6IjM3ZzU4Zk94cEcrb0VXenZHbmdTVnc9PSIsInZhbHVlIjoiWXVcL2s0ZDBvYmRxOTNNbStJKzhZZXBmdEthakpZWXBXdVd0aXJFRUdcL2I0PSIsIm1hYyI6ImNhZjFkYTYzNzA5OTMwOTQyMDVkMGU0YTkzNzc1NjkzMjg1Y2RiNjFlNzJlMGJkOTM2YTM1OTU5MDQxZTM5ZjQifQ==',
         'task_id' => '6811596'
     ],
@@ -12,7 +12,7 @@ return [
         'project6' => [
             'id' => '2406402',
             'url' => 'https://project6.harvestapp.com',
-            'user' => 'mail@2tabs.com',
+            'user' => 'mail@johannesschmidt.ca',
             'password' => 'eyJpdiI6IjM3ZzU4Zk94cEcrb0VXenZHbmdTVnc9PSIsInZhbHVlIjoiWXVcL2s0ZDBvYmRxOTNNbStJKzhZZXBmdEthakpZWXBXdVd0aXJFRUdcL2I0PSIsIm1hYyI6ImNhZjFkYTYzNzA5OTMwOTQyMDVkMGU0YTkzNzc1NjkzMjg1Y2RiNjFlNzJlMGJkOTM2YTM1OTU5MDQxZTM5ZjQifQ==',
             'name' => 'Project 6 Design',
         ]
--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,5 @@
 
 
 Route::get('/', function () {
-
-    $harvestClient = new \BestIt\Harvest\Client('https://2tabs.harvestapp.com', 'mail@2tabs.com', 'ees2aeGh123');
-
-//    $harvestClient = new \BestIt\Harvest\Client('https://project6.harvestapp.com', 'mail@2tabs.com', 'ees2aeGh123');
-
-//    $harvestClient->timesheet()->
-
-    // Find all projects for this client.
-    $projects = $harvestClient->projects()->findByClientId(2406402);
-    d($projects);
-    $timesheets = $harvestClient->timesheet()->all(true, new DateTime('-1 day'));
-//    d($timesheets);
-
     return view('home');
 });
